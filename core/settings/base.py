33340b7ee259a1d934101eb05c859163ea468740
islam0122 <duishobaevislam01@gmaul.com>
2025-02-16 00:54:33 +0600

    update date -> jwt token

--- core/settings/base.py
+++ core/settings/base.py
@@ -93,7 +93,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=48),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
