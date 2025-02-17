a62eb513ed49a18d2967872717a0a96f34c0037a
islam0122 <duishobaevislam01@gmaul.com>
2025-02-17 22:54:41 +0600

    update date -> jwt token

--- app/Groups/views.py
+++ app/Groups/views.py
@@ -3,19 +3,27 @@ from rest_framework import permissions
 from .models import Student, Group
 from .serializers import StudentSerializer, GroupSerializer
 
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    """
+    Разрешает просмотр всем, но изменять данные могут только авторизованные пользователи.
+    """
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
+            return True
+        return request.user and request.user.is_authenticated
+
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(create_user=self.request.user)
 
-
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(create_user=self.request.user)
--- app/quiz/views.py
+++ app/quiz/views.py
@@ -2,13 +2,24 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Topic, Question
 from .serializers import TopicSerializer, QuestionSerializer
+from rest_framework import permissions
+
+
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    """
+    Разрешает просмотр всем, но изменять данные могут только авторизованные пользователи.
+    """
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
+            return True
+        return request.user and request.user.is_authenticated
 
 
 class TopicViewSet(ModelViewSet):
     """ViewSet для тем"""
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         """Автоматически устанавливает текущего пользователя как создателя темы"""
@@ -19,7 +30,7 @@ class QuestionViewSet(ModelViewSet):
     """ViewSet для вопросов"""
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         """Автоматически устанавливает текущего пользователя как создателя вопроса"""
