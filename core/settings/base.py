ca5d050e2759137fb29d161d55a02d3a7319aca0
islam0122 <duishobaevislam01@gmaul.com>
2025-02-22 01:58:53 +0600

    fix -> jazzmin

--- core/settings/base.py
+++ core/settings/base.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
 
     'djoser',
@@ -120,3 +119,49 @@ else:
 #     INTERNAL_IPS = ['127.0.0.1','localhost',]
 #     MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
+JAZZMIN_SETTINGS = {
+    "site_title": "EduQuiz",  # Заголовок сайта
+    "site_header": "Админка EduQuiz",  # Заголовок на главной странице админки
+    "site_brand": "EduQuiz",  # Логотип или название бренда на странице
+    "welcome_sign": "Добро пожаловать в админку EduQuiz!",  # Приветственное сообщение
+    "topmenu_links": [
+        {"name": "Главная", "url": "/admin/", "permissions": ["auth.view_user"]},
+    ],
+
+    "custom_links": {
+        "eduquiz": [
+            {"name": "Перейти на сайт", "url": "/", "icon": "fas fa-home", "permissions": []},
+        ]
+    },
+}
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-teal",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "simplex",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-outline-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
