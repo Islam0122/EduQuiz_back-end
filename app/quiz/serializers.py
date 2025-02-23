6cfb596afe2dacb586289fd7c08b1e571800dcd9
islam0122 <duishobaevislam01@gmaul.com>
2025-02-24 02:37:58 +0600

    fix -> quiz

--- app/quiz/serializers.py
+++ app/quiz/serializers.py
@@ -3,13 +3,9 @@ from .models import Topic, Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-    """Сериализатор для вопросов с краткой информацией о теме"""
-    topic_name = serializers.CharField(source="topic.name", read_only=True)  # Название темы
-    topic_id = serializers.IntegerField(source="topic.id", read_only=True)  # ID темы
-
     class Meta:
         model = Question
-        fields = ["id", "text","image", "option_a", "option_b", "option_c", "option_d", "correct_answer", "topic_id", "topic_name"]
+        fields = "__all__"
 
 
 class TopicSerializer(serializers.ModelSerializer):
